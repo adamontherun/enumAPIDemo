50da8e8d4b14eb433361b260fbc20d2691759de2
adam smith <adaminhonolulu@gmail.com>
2017-05-11 15:49:37 -1000

    initial commit

---
+++ Podfile
@@ -0,0 +1,4 @@
+target 'Endpoint' do
+  use_frameworks!
+  pod 'Alamofire', '~> 4.4'
+end
---
+++ Pods/Target Support Files/Pods-Endpoint/Pods-Endpoint-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Endpoint : NSObject
+@end
+@implementation PodsDummy_Pods_Endpoint
+@end
